0a9fe81b2f74f6e8dd70e55f58b209b6ec148261
svetliosa <svetlio11plam@gmail.com>
2025-02-22 14:21:20 +0200

    [frontend] Bind faUser icon in component

--- apps/ecommerce-app-frontend/src/app/app.component.ts
+++ apps/ecommerce-app-frontend/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FaConfig, FaIconComponent, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
-  imports: [RouterModule],
-  selector: 'app-root',
+  imports: [RouterModule, FaIconComponent],
+  selector: 'ecom-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ecommerce-app-frontend';
+export class AppComponent implements OnInit {
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
