c7cdc6ba1ee278712408b6cd45d213e41e52331a
svetliosa <svetlio11plam@gmail.com>
2025-02-23 15:33:06 +0200

    [frontend] Add Help, Company and Legal category with the with the relevant links in the footer component
     - Help
        - Shipping
        - Returns + Exchanges
        - FAQ
        - Compare
     - Company
       - About us
       - Contact
       - Jobs
       - Press kit
     - Legal
       - Terms of use
       - Privacy policy
       - Cookie policy

--- apps/ecommerce-app-frontend/src/app/shared/font-awesome-icons.ts
+++ apps/ecommerce-app-frontend/src/app/shared/font-awesome-icons.ts
@@ -1,7 +1,12 @@
 import { IconDefinition } from "@fortawesome/angular-fontawesome";
-import { faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faUser, faTruckFast } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 export const fontAwesomeIcons: IconDefinition[] = [
     faUser,
-    faCartShopping
+    faCartShopping,
+    faTruckFast,
+    faYoutube,
+    faFacebook,
+    faTwitter
 ];
